e78544ac920a553547eaf80d685a7b4a77de4b15
K.D. Hubbard <hubbard.kd@gmail.com>
2023-02-22 14:13:11 -0700

    add four methods with commented explanation

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ dogs = ["Wolfie", "Pirate", "Ava", "Taro Boba", "Junie"]
 age_dog_years = [14, 7, 28, 21, 35]
 cups_per_day = [1.5, 2, 2.5, 1.5, 2]
 good_boy_girl = [true, true, true, true, true]
+
+# The length method will count the number of elements in the array.
+dogs.length
+
+# The pop method will remove the last element in the array.
+cups_per_day.pop
+
+# The push method will move the last element to the first position in the array. 
+age_dog_years.push
+
+# The shift method will remove the first element in the array.
+good_boy_girl.shift
