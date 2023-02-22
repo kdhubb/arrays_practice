96c259a25cacc91ffcc0f14ffabaa6566077a81f
K.D. Hubbard <hubbard.kd@gmail.com>
2023-02-22 14:25:37 -0700

    add extra method with explanation

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,4 @@
-dogs = ["Wolfie", "Pirate", "Ava", "Taro Boba", "Junie"]
+dogs = ["Wolfie", "Pirate", "Ava", "Taro Boba", "Junie", "Amanda"]
 age_dog_years = [14, 7, 28, 21, 35]
 cups_per_day = [1.5, 2, 2.5, 1.5, 2]
 good_boy_girl = [true, true, true, true, true]
@@ -14,3 +14,8 @@ age_dog_years.push
 
 # The shift method will remove the first element in the array.
 good_boy_girl.shift
+
+# Index positions refer to the position that an element sits in an array. For example, under the variable dogs, the element "Wolfie" sits at index position 0, "Pirate" sits at 1, and so on.
+
+# The min method will output the element with lowest value, or the first element in alphabetical order.
+dogs.min
